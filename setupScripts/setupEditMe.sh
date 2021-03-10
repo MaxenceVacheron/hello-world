d9947cb66675dd149b82ed10416b39968ca8eb7d
Maxence Vacheron <vacheronmaxence@gmail.com>
2021-03-10 17:20:59 +0100

    Adding support for better editable setup

--- setupScripts/setupEditMe.sh
+++ setupScripts/setupEditMe.sh
@@ -1,4 +1,17 @@
 #bin/bash
+
+
+# Backing up old .zshrc file
+cp ~/.zshrc ~/.zshrc.bak
+
+# Updating .zshrc file
+cp zshrc ~/.zshrc
+
+# Dev tools include a full upgrade of the distro, and additional install of git, vim and tree.
 sh src/setupDevTools.sh
-sh src/setupOhMyZSH.sh
+sh src/cleanInstallDocker.sh
 #sh src/setupGestures.sh
+sh src/setupOhMyZSH.sh
+
+# Sourcong zshrc file
+#source ~/.zshrc
--- setupScripts/src/setupOhMyZSH.sh
+++ setupScripts/src/setupOhMyZSH.sh
@@ -1,4 +1,7 @@
 #bin/bash
 
-wget https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh
-sh install.sh
+#wget https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh
+#mv install.sh ~/install.sh 
+#cd
+#sh install.sh
+echo "Can't install OMZSH, please do it manually"
