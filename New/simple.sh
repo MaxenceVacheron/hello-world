5165b50bc5fc2a6e63b40c06f75743b7c85b4c2b
Maxence Vacheron <max@pop-os.localdomain>
2021-10-23 22:45:31 +0200

    Clean

--- New/simple.sh
+++ New/simple.sh
@@ -1,13 +1,13 @@
-sudo apt install -y zsh tree traceroute htop
+sudo apt install -y vim zsh tree traceroute htop
 
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
 curl -fsSL https://get.docker.com -o get-docker.sh
-sh get-docker.sh
+
 sudo apt-get -y install docker-compose
 
-#groupadd docker
 sudo usermod -aG docker $USER
 newgrp docker
+
 docker run hello-world
 
