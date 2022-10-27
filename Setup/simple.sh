23faa9183632a6acdd88c5be2394ae20b074c539
Maxence Vacheron <vacheronmaxence@gmail.com>
2022-10-27 11:40:19 +0200

    Cleaning and added vlc and neofetch

--- Setup/simple.sh
+++ Setup/simple.sh
@@ -1,4 +1,4 @@
-sudo apt install -y vim zsh tree traceroute htop ffmpeg pip curl
+sudo apt install -y vim zsh tree traceroute htop ffmpeg pip curl vlc neofetch
 
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
---
+++ conf/conf-gen/mouseWheelScroll.sh
@@ -0,0 +1,1 @@
+sudo apt-get install imwheel -y && bash <(curl -s http://www.nicknorton.net/mousewheel.sh)
